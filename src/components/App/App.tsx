427261d0e14b8f9cc7b18d73a3bcae9062554faf
Kateryna <katekirshe95@gmail.com>
2026-03-22 12:16:52 +0200

    add hw react 2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import css from './App.module.css'
 import type { Vote, VoteType } from '../../types/votes'
 import VoteOptions from '../VoteOptions/VoteOptions'
 import VoteStats from '../VoteStats/VoteStats'
+import Notification from '../Notification/Notification'
 
 function App() {
   const [votes, onChangeState] = useState<Vote>({ good: 0, neutral: 0, bad: 0 })
@@ -28,13 +29,13 @@ function handleVote(type: VoteType) {
 
   console.log(votes);
   const totalVotes = votes.good + votes.neutral + votes.bad
-  const positiveRate = totalVotes? +((votes.good*100)/totalVotes).toFixed(2) : 0
+  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0
 
   return (
   <div className={css.app}>
       <CafeInfo />
-      <VoteOptions canReset={true} onReset={resetVotes} onVote={handleVote} />
-      <VoteStats votes={ votes } totalVotes={ totalVotes } positiveRate={ positiveRate }/>
+      <VoteOptions canReset={ totalVotes > 0 } onReset={resetVotes} onVote={handleVote} />
+      { totalVotes ? <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} /> : <Notification />}
   </div>
   )
 }
